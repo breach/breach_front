4f382f8b7f73c0b16a0702fab0bb3cdd03224cdc
Stanislas Polu <polu.stanislas@gmail.com>
2014-05-04 18:45:22 -0700

    Front initial implementation

--- index.js
+++ index.js
@@ -16,9 +16,44 @@ var util = require('util');
 var fs = require('fs');
 var http = require('http');
 
+var common = require('./lib/common.js');
+
 var app = express();
 
 var setup = function() {
+  //
+  // ### _JSON ROUTES_
+  //
+  app.post('/auth/signup',              require('./routes/auth.js').post_signup);
+  app.post('/auth/signin',              require('./routes/auth.js').post_signin);
+
+  app.post('/auth/verify/request',      require('./routes/auth.js').post_verify_request);
+  app.get( '/auth/verify/:email/:code', require('./routes/auth.js').get_verify_code);
+};
+
+
+// INIT & START
+common.log.out('Breach: front [Started]');
+
+common.PG_URL = process.env['BREACH_FRONT_PG_URL'] ||
+  'postgres://dummy:dummy@localhost/breach_front';
+
+common.PORT = process.env['BREACH_FRONT_PORT'] ?
+  parseInt(process.env['BREACH_FRONT_PORT'], 10) : 0;
+
+common.SECRET = process.env['BREACH_FRONT_SECRET'] || 'dummy';
+
+common.CIO_SITE_ID = process.env['BREACH_CIO_SITE_ID'] || 'dummy';
+common.CIO_SECRET_KEY = process.env['BREACH_CIO_SECRET_KEY'] || 'dummy';
+
+console.log('PG_URL: ' + common.PG_URL);
+common.pg = new (require('pg').Client)(common.PG_URL);
+common.pg.connect(function(err) {
+  if(err) {
+    console.log('HERE');
+    return common.fatal(err);
+  }
+
   /* App Configuration */
   app.use('/', express.static(__dirname + '/app'));
   app.use(require('body-parser')());
@@ -34,27 +69,19 @@ var setup = function() {
     });
   });
 
-  //
-  // ### _JSON ROUTES_
-  //
+  /* Setup */
+  setup();
 
-  app.post('/auth/signup',            require('./routes/auth.js').post_signup);
-  app.get( '/auth/signin',            require('./routes/auth.js').post_signin);
-};
+  var http_srv = http.createServer(app).listen(common.PORT);
+  http_srv.on('listening', function() {
+    var port = http_srv.address().port;
+    common.log.out('HTTP Server started on port: ' + common.PORT);
+  });
 
-// INIT & START
-common.log.out('breach_front [Started]');
-
-/* Setup */
-setup();
-
-common.PORT = process.env['TEABAG_TABLE_PORT'] ?
-  parseInt(process.env['TEABAG_TABLE_PORT'], 10) : 0;
-
-var http_srv = http.createServer(my.app).listen(common.PORT);
-http_srv.on('listening', function() {
-  var port = http_srv.address().port;
-  common.log.out('HTTP Server started on port: ' + common.PORT);
+  common.cio = new (require('node-customer.io'))(
+    common.CIO_SITE_ID,
+    common.CIO_SECRET_KEY
+  );
 });
 
 // SAFETY NET (kills the process and the spawns)
