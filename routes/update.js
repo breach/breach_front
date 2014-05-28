86b8b93d77705dafe3219a1e1ddb7ab6a4fd8bde
Stanislas Polu <polu.stanislas@gmail.com>
2014-05-27 17:48:46 -0700

    Added signature files URLs

--- routes/update.js
+++ routes/update.js
@@ -6,6 +6,7 @@
  * @author: spolu
  *
  * @log:
+ * - 2014-05-27 spolu   Add signature file URLs
  * - 2014-05-15 spolu   Creation
  */
 "use strict"
@@ -27,16 +28,16 @@ exports.get_update = function(req, res, next) {
   var update = {
     'darwin': {
       'ia32': {
-        md5: '104d78e4c94cf33fcbd382a7762ec93d',
         version: '0.3.5-alpha.1',
-        url: 'https://s3.amazonaws.com/breach_releases/breach/v0.3.5-alpha.1/breach-v0.3.5-alpha.1-darwin-ia32.tar.gz'
+        url: 'https://s3.amazonaws.com/breach_releases/breach/v0.3.5-alpha.1/breach-v0.3.5-alpha.1-darwin-ia32.tar.gz',
+        signature: 'https://s3.amazonaws.com/breach_releases/breach/v0.3.5-alpha.1/breach-v0.3.5-alpha.1-darwin-ia32.tar.gz.sha1sum.asc'
       }
     },
     'linux': {
       'x64': {
-        md5: '9719fe450db47edc97b0382c1095c82f',
         version: '0.3.5-alpha.1',
-        url: 'https://s3.amazonaws.com/breach_releases/breach/v0.3.5-alpha.1/breach-v0.3.5-alpha.1-linux-x64.tar.gz'
+        url: 'https://s3.amazonaws.com/breach_releases/breach/v0.3.5-alpha.1/breach-v0.3.5-alpha.1-linux-x64.tar.gz',
+        signature: 'https://s3.amazonaws.com/breach_releases/breach/v0.3.5-alpha.1/breach-v0.3.5-alpha.1-linux-x64.tar.gz.sha1sum.asc'
       }
     }
   };
